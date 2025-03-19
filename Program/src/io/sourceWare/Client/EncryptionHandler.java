9812e939654b4dce6ca0d2ddae37685a877ea0ee
Rafael Penacho <rafapenacho@gmail.com>
2025-03-19 20:48:06 +0000

    fixed adress not working

---
+++ Program/src/io/sourceWare/Client/EncryptionHandler.java
@@ -0,0 +1,4 @@
+package io.sourceWare.Client;
+
+public class EncryptionHandler {
+}
--- Program/src/io/sourceWare/PlayGround.java
+++ Program/src/io/sourceWare/PlayGround.java
@@ -1,4 +1,7 @@
 package io.sourceWare;
 
 public class PlayGround {
+    public static void main(String[] args) {
+
+    }
 }
