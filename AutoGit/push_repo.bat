630ad4c1e8139dc58126a9d57cf1aea902f03916
Rafael Penacho <rafapenacho@gmail.com>
2025-03-20 19:53:07 +0000

    auto git update 6, minor changes relating to logs

--- AutoGit/push_repo.bat
+++ AutoGit/push_repo.bat
@@ -26,9 +26,11 @@ if /I "%confirm%" NEQ "Y" (
 :: Run Git commands and log output
 echo [%DATE% %TIME%] Running git add, commit, and push... >> git_log.txt
 git add . >> git_log.txt 2>&1
+echo [%DATE% %TIME%] Committing with message: "%commitMessage%" >> git_log.txt
 git commit -m "%commitMessage%" >> git_log.txt 2>&1
 git push origin %branchName% >> git_log.txt 2>&1
 echo [%DATE% %TIME%] Git push completed. >> git_log.txt
+
 
 :: Check if log file is empty, delete if it is
 for %%A in (git_log.txt) do if %%~zA==0 (
