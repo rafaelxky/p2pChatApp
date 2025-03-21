828b9938de8c7296bab6e697942792d6cd1f0b4a
Rafael Penacho <rafapenacho@gmail.com>
2025-03-21 15:10:15 +0000

    small changes to project structure

--- Program/src/io/sourceWare/program/client/Client.java
+++
@@ -1,5 +0,0 @@
-package io.sourceWare.program.client;
-
-public class Client {
-    // handles client logic
-}
---
+++ Program/src/io/sourceWare/program/client/model/connections/Client.java
@@ -0,0 +1,5 @@
+package io.sourceWare.program.client.model.connections;
+
+public class Client {
+    // handles client logic
+}
