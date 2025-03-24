9b1be5c9faf67e64aad9e827c80fcf3719c0e425
Rafael Penacho <rafapenacho@gmail.com>
2025-03-24 19:45:17 +0000

    wrote upd connection tests, added a volatile variable to store the incoming messages in Connection Data

---
+++ Program/src/io/sourceWare/Tests/PopupsTest.java
@@ -0,0 +1,10 @@
+package io.sourceWare.Tests;
+
+import io.sourceWare.program.client.view.Popus;
+
+public class PopupsTest {
+    public static void main(String[] args) {
+        Popus.popup("Test popup", "Test popup text!");
+        Popus.unknownConnection();
+    }
+}
