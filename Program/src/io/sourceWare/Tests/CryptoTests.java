d76c118a7131d6a352bcf462c01533f20398b1e7
Rafael Penacho <rafapenacho@gmail.com>
2025-03-31 14:29:06 +0100

    added 1 way encryption using SHA-256. Added salt generation and implementation. Tests for encryption.

--- Program/src/io/sourceWare/Tests/CryptoTests.java
+++ Program/src/io/sourceWare/Tests/CryptoTests.java
@@ -2,13 +2,38 @@ package io.sourceWare.Tests;
 
 import io.sourceWare.program.client.model.crypto.EncryptionHandler;
 
+import java.security.NoSuchAlgorithmException;
+
 public class CryptoTests {
     public static void main(String[] args) {
         System.out.println(encryptionTest().equals("zoVIE66EIk8op3GyzV2/Cw==") ? " - encryption success" : " - encryption failed");
         System.out.println("Encrypted message: " + encryptionTest());
+        System.out.println();
 
         System.out.println(decryptionTest().equals("Hello World") ? " - decryption test success" : " - decryption test failed");
         System.out.println("Decrypted message: " + decryptionTest());
+        System.out.println();
+
+
+        System.out.println(permanentEncryptionTestNoSalt().equals("a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e") ? " - permanentEncryptionTestNoSalt, success" : " - permanentEncryptionTestNoSalt, failed");
+        System.out.println(permanentEncryptionTestNoSalt());
+        System.out.println();
+
+
+        System.out.println(permanentEncryptionTestStaticSalt().equals("4fb8894f57c227081e6da59ee7771a55082a88137b7ddf5b423448ae4beb1ca0") ? " - permanentEncryptionTestWithStaticSalt, success" : " - permanentEncryptionTestWithStaticSalt, failed");
+        System.out.println(permanentEncryptionTestStaticSalt());
+        System.out.println();
+
+
+        System.out.println(addSaltTest().equals("[72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 83, 97, 108, 116]") ? " - addSaltTest, success" : " - addSaltTest, failed");
+        System.out.println(addSaltTest());
+        System.out.println();
+
+
+        System.out.println("Generated salt: " + generateSalt());
+        System.out.println();
+
+
     }
 
 
@@ -29,10 +54,30 @@ public class CryptoTests {
         }
     }
 
-    public static String permanentEncryptionTest(){
-        EncryptionHandler.permanentEncryption();
-        System.out.println("Unimplemented feature - permanentEncryption");
+    public static String permanentEncryptionTestNoSalt(){
+        try {
+            return EncryptionHandler.permanentEncryption("Hello World");
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println(e.getMessage());
+        }
         return null;
+    }
+
+    public static String permanentEncryptionTestStaticSalt(){
+        try {
+            return EncryptionHandler.permanentEncryption(EncryptionHandler.addSalt("Hello World", "salt"));
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public static String addSaltTest(){
+        return EncryptionHandler.addSalt("Hello World", "Salt");
+    }
+
+    public static String generateSalt(){
+        return EncryptionHandler.generateSalt();
     }
 
 }
