66d1f5aebc2ce498ab6b701abc4737ecb9c148d0
Rafael Penacho <rafapenacho@gmail.com>
2025-03-31 14:29:06 +0100

    added 1 way encryption using SHA-256. Added salt generation and implementation. Tests for encryption.

--- Program/src/io/sourceWare/program/client/model/crypto/EncryptionHandler.java
+++ Program/src/io/sourceWare/program/client/model/crypto/EncryptionHandler.java
@@ -1,9 +1,12 @@
 package io.sourceWare.program.client.model.crypto;
+
 import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.Base64;
 
@@ -14,7 +17,7 @@ public class EncryptionHandler {
     public static String encrypt(String message, String key) throws Exception {
 
         // create secret key from key
-        byte[] keyBytes = Arrays.copyOf(key.getBytes(StandardCharsets.UTF_8),SIZE);
+        byte[] keyBytes = Arrays.copyOf(key.getBytes(StandardCharsets.UTF_8), SIZE);
         SecretKeySpec secretKey = new SecretKeySpec(keyBytes, "AES");
 
         // create cipher object
@@ -30,10 +33,10 @@ public class EncryptionHandler {
         return Base64.getEncoder().encodeToString(encryptedMessage);
     }
 
-    public static String decrypt (String encryptedMessage, String key) throws Exception{
+    public static String decrypt(String encryptedMessage, String key) throws Exception {
 
         // Create a secret key from key
-        byte[] keyBytes = Arrays.copyOf(key.getBytes(StandardCharsets.UTF_8),SIZE);
+        byte[] keyBytes = Arrays.copyOf(key.getBytes(StandardCharsets.UTF_8), SIZE);
         SecretKeySpec secretKey = new SecretKeySpec(keyBytes, "AES");
         // Create cipher object
         Cipher cipher = Cipher.getInstance("AES");
@@ -52,9 +55,36 @@ public class EncryptionHandler {
 
     // add a permanent encription that cant be decripted
 
-    public static String permanentEncryption(){
-        System.out.println("Unimplemented feature - permanentEncryption");
-       return null;
+    public static String permanentEncryption(String input) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+        byte[] hashByte = md.digest(input.getBytes(StandardCharsets.UTF_8));
+
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : hashByte) {
+            hexString.append(String.format("%02x", b));
+        }
+
+        return hexString.toString();
     }
-    
+
+    public static String addSalt(String in, String salt) {
+        byte[] saltArr = salt.getBytes();
+
+        byte[] inputBytes = in.getBytes(StandardCharsets.UTF_8);
+        byte[] saltedIn = new byte[inputBytes.length + saltArr.length];
+
+        System.arraycopy(inputBytes, 0, saltedIn, 0, inputBytes.length);
+        System.arraycopy(saltArr, 0, saltedIn, inputBytes.length, saltArr.length);
+
+
+        return Arrays.toString(saltedIn);
+    }
+
+    public static String generateSalt() {
+        byte[] salt = new byte[16];
+        new SecureRandom().nextBytes(salt);
+        return Arrays.toString(salt);
+    }
+
 }
