5e537b163fb28e6bafe5fe0e1d8530317f68cde7
Rafael Penacho <rafapenacho@gmail.com>
2025-04-02 17:37:52 +0100

    html changes to the shows

--- Program/src/io/sourceWare/program/client/view/html/show/JavaFXShow.java
+++ Program/src/io/sourceWare/program/client/view/html/show/JavaFXShow.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
+import javafx.event.EventTarget;
 
 import java.io.IOException;
 import java.net.URL;
@@ -12,40 +13,29 @@ import java.net.URL;
 public class JavaFXShow extends Application implements Show {
 
     @Override
-    public void start(Stage stage) {
-        // todo: fix the javafx imports
+    public void start(Stage primaryStage) {
         // Create a WebView to display HTML content
         WebView webView = new WebView();
         WebEngine webEngine = webView.getEngine();
 
-        // Try to load the HTML file
-        URL url = getClass().getResource("/io/sourceWare/program/client/view/html/index.html");
+        // Load an HTML file from the classpath (or use a URL)
+        // This can be your index.html or any valid HTML file
+        String htmlFilePath = "file:///path/to/your/index.html";  // Modify the path
+        webEngine.load(htmlFilePath);
 
-        if (url != null) {
-            // If the file is found, load it into the WebView
-            webEngine.load(url.toExternalForm());
-            System.out.println("Serving file from: " + url);
-        } else {
-            // If the file is not found, display an error message
-            System.err.println("Error: File not found in classpath: /io/sourceWare/program/client/view/html/index.html");
-            webEngine.loadContent("<html><h1 style='color:red;'>Error loading page</h1></html>");
-        }
+        // Alternatively, load HTML content as a string directly
+        String htmlContent = "<html><body><h1>Hello, JavaFX!</h1><p>This is a paragraph.</p></body></html>";
+        webEngine.loadContent(htmlContent);
 
-        // Create a scene with the WebView and set the window size
+        // Set up the scene with the WebView
         Scene scene = new Scene(webView, 800, 600);
-        stage.setTitle("p2pChatApp");
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setTitle("JavaFX HTML Viewer");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
-    // Implement the show method from the Show interface
     @Override
     public void show() {
         launch();
-    }
-
-    // Main method to launch the JavaFX application
-    public static void main(String[] args) {
-        launch(args);
     }
 }
