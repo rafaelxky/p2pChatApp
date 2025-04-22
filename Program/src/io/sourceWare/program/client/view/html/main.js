d0d7c3543044a3f1a5fa51b0e893f2babd56e78a
Rafael Penacho <rafapenacho@gmail.com>
2025-04-22 20:25:55 +0100

    final daily push

--- Program/src/io/sourceWare/program/client/view/html/main.js
+++ Program/src/io/sourceWare/program/client/view/html/main.js
@@ -6,6 +6,8 @@ import http from 'http';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.disableHardwareAcceleration();
+
 let mainWindow;
 
 function startHttpServer() {
